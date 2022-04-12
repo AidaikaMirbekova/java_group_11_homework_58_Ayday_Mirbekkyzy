682c0259b634aa00265a142616d63752c54ae4a7
AydaykaMirbekova <aidaika.mirbekova@gmail.com>
2022-04-12 15:35:51 +0600

    init commit

---
+++ src/main/resources/static/script.js
@@ -0,0 +1,56 @@
+// Создан объект каласса Пользователя,
+const User = {
+    id: 1,
+    email: 'mike@gmail.com',
+    username: "mike",
+    login: "Mike1",
+    autorized: true
+};
+
+// Создан объект класса Постов. я решила связать user  и  post просто указав ссылку на сам объект, путем его объявления.
+// Так как он будет в будущем ссылатся именно на данного пользователя.
+const Post = {
+    id: 1,
+    author: User,
+    image: 'horror.jpg',
+    description: 'Hello',
+    date: "2022-05-04"};
+
+//Создан объект Класса коментарий. Он также будет ссылатся единственного пользователя.
+const Comment = {
+    id: 1,
+    author: User,
+    post: Post,
+    commentText: 'Good foto)',
+    date: "2022-05-05"
+};
+
+// Массив постов
+let postArray = [
+    {id: 1, author: User, image: 'summer.jpg', description: 'So sunny'},
+    {id: 2, author: User, image: 'winter.jpg', description: 'So cold'},
+    {id: 3, author: User, image: 'autumn.jpg', description: 'So wildly'},
+    {id: 4, author: User, image: 'spring.jpg', description: 'So green'},];
+
+// Добавление новых постов в массив постов
+function addPosts(post) {
+    let newPost = postArray.push(post)
+}
+
+let newPostadd = {id: 12, author: User, image: 'cat.kg', description: 'hello'};
+addPosts(newPostadd);
+
+// функция сменяющий статус объекта User с неавторизованного на авторизованный и обратно.
+function changeAutorizedStatus(user) {
+    user.autorized = user.autorized !== true;
+};
+
+changeAutorizedStatus(User);
+
+
+
+
+
+
+
+
